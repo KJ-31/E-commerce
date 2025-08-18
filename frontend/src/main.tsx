f52a342ace67f870d29459fccf59cf4c562d6ef0
Sanghwa Bae <baesanghwa1214@gmail.com>
2025-08-18 20:59:35 +0900

    Revert "feat: product_page"
    This reverts commit a7c3219dd12dd2a2d90e753eb8fac867a9b31034.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import ElevenStreetHome from "./11st-Home.tsx";
-import ProductDetail from "./productdetail.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* 홈 페이지 */}
-        <Route path="/" element={<ElevenStreetHome />} />
-        {/* 상품 상세 페이지 */}
-        <Route path="/product/:id" element={<ProductDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <ElevenStreetHome />
   </React.StrictMode>
 );
